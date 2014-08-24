e1b84cbcf7b257ce9c33638b95f99758362810c2
SaneaSaniuta <colodin@mail.ru>
2014-08-24 20:37:10 +0300

    Swift TabBar Example
    Swift TabBar Example

---
+++ TabBar/TabBar/ScreenOne.swift
@@ -0,0 +1,22 @@
+//
+//  ScreenOne.swift
+//  TabBar
+//
+//  Created by Colodin on 31.07.14.
+//  Copyright (c) 2014 Alexandru Colodin. All rights reserved.
+//
+
+import UIKit
+class ScreenOne: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Screen One")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ TabBar/TabBar/ScreenTwo.swift
@@ -0,0 +1,23 @@
+//
+//  ScreenTwo.swift
+//  TabBar
+//
+//  Created by Colodin on 31.07.14.
+//  Copyright (c) 2014 Alexandru Colodin. All rights reserved.
+//
+
+import UIKit
+class ScreenTwo: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Screen Two")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
